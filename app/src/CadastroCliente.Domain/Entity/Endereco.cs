60938745099c7f160348a7cbf5ec515c4d3562fa
Ayrton Rodrigues <92091259+Ayrton-Rodrigues@users.noreply.github.com>
2023-08-05 16:56:33 -0300

    feat: criacao de entidades cliente e endereco

---
+++ app/src/CadastroCliente.Domain/Entity/Endereco.cs
@@ -0,0 +1,28 @@
+﻿namespace CadastroCliente.Domain.Entity;
+
+public class Endereco : Entity
+{
+    public Guid ClienteId { get; private set; }
+    public string Numero { get; private set; }
+
+    public string Bairro { get; private set; }
+
+    public string Cidade { get; private set; }
+
+    public string Estado { get; private set; }
+
+    public string Cep { get; private set; }
+
+    public Endereco(string numero, string bairro, string cidade, string estado, string cep, Guid clienteId)
+    {
+
+        Numero = numero;
+        Bairro = bairro;
+        Cidade = cidade;
+        Estado = estado;
+        Cep = cep;
+        ClienteId = clienteId;
+    }
+}
+
+
