d83fe750e4b2fbfb95c4469203c6f84590890b57
Ayrton Rodrigues <92091259+Ayrton-Rodrigues@users.noreply.github.com>
2023-08-06 12:24:06 -0300

    feat: criacao dos mappings e repositorio de cliente

---
+++ app/src/CadastroCliente.Data/Mappings/ClienteMapping.cs
@@ -0,0 +1,35 @@
+﻿using CadastroCliente.Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroCliente.Data.Mappings
+{
+    public class ClienteMapping : IEntityTypeConfiguration<Cliente>
+    {
+        public void Configure(EntityTypeBuilder<Cliente> builder)
+        {
+            builder.HasKey(k => k.Id);
+
+            builder.Property(x => x.Nome)
+                .IsRequired();
+
+
+            builder.Property(x => x.Email)
+                .IsRequired();
+
+            builder.Property(x => x.DataCadastro)
+                .IsRequired();
+
+            builder.HasOne(x => x.Endereco)
+                .WithOne(x => x.Cliente)
+                .HasForeignKey<Endereco>(x => x.ClienteId);
+        }
+    }
+}
+
+ 
