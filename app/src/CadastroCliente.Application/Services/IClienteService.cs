fb42f13369824359a73ce65805e1585f0524352c
Ayrton Rodrigues <92091259+Ayrton-Rodrigues@users.noreply.github.com>
2023-08-06 12:27:14 -0300

    feat: criacao de config automapper, service de cliente e viewModels no application

---
+++ app/src/CadastroCliente.Application/Services/IClienteService.cs
@@ -0,0 +1,23 @@
+﻿using CadastroCliente.Application.ViewModels;
+using CadastroCliente.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroCliente.Domain.Interfaces
+{
+    public interface IClienteService
+    {
+        Task<IEnumerable<ClienteViewModel>> ObterTodosClientes();
+
+        Task<ClienteViewModel> ObterClientePorId(Guid id);
+
+        Task<bool> Adicionar(ClienteViewModel cliente);
+        Task<bool> Atualizar(ClienteViewModel cliente);
+        Task<bool> Remover(ClienteViewModel cliente);
+        
+        void AtualizarEndereco(Endereco endereco);
+    }
+}
---
+++ app/src/CadastroCliente.Application/ViewModels/ClienteViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CadastroCliente.Application.ViewModels;
+
+public class ClienteViewModel
+{
+     
+    [Key]
+    public Guid Id { get; set; }
+    
+    
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [StringLength(200, ErrorMessage = "O campo {0} precisa conter entre {2} e {1} caracter", MinimumLength = 2)]
+     public required string Nome { get; set; }    
+    
+    
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [StringLength(50, ErrorMessage = "O campo {0} precisa conter entre {2} e {1} caracter", MinimumLength = 2)]
+    public required string Email { get; set; }
+
+    public DateTime DataCadastro { get; set; } 
+    public DateTime? DataAtualizacao { get; set; }
+
+    public EnderecoViewModel? EnderecoViewModel { get; set; }
+
+
+}
+
