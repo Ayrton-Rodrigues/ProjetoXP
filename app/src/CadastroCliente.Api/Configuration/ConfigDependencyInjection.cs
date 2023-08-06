2efda6e7390022edee0b15a28a4446c31dbc6cf6
Ayrton Rodrigues <92091259+Ayrton-Rodrigues@users.noreply.github.com>
2023-08-06 12:28:39 -0300

    feat: criacao de controller de cliente com operacoes crud, configuracao para resolver as DI

---
+++ app/src/CadastroCliente.Api/Configuration/ConfigDependencyInjection.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using CadastroCliente.Application.AutoMapper;
+using CadastroCliente.Application.Services;
+using CadastroCliente.Data;
+using CadastroCliente.Data.Repository;
+using CadastroCliente.Domain.Interfaces;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Collections.Generic;
+
+namespace CadastroCliente.Api.Configuration
+{
+    public static class ConfigDependencyInjection
+    {
+        public static IServiceCollection ResolveDependencies(this IServiceCollection services)
+        {
+            services.AddScoped<CadastroDbContext>();
+
+            var mappingConfig = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new AutoMapperConfig());
+            });
+
+            IMapper mapper = mappingConfig.CreateMapper();
+            services.AddSingleton(mapper);            
+
+            services.AddScoped<IClienteRepository, ClienteRepository>();
+
+            services.AddScoped<IClienteService, ClienteService>();
+            
+
+
+            return services;
+        }
+    }
+}
